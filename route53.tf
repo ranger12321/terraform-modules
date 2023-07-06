48ee5630401e8c4f5276429cc5d316ed2aa81573
ranger12321 <102017357+ranger12321@users.noreply.github.com>
2023-07-06 11:59:31 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,13 +1,13 @@
 # Create Route53 hosted zone
 resource "aws_route53_zone" "my_hosted_zone" {
-  name = "mydomain.com"  # Replace with your desired domain name
+  name = var.domain_name  # Replace with your desired domain name
 }
 
 # Create DNS record
 resource "aws_route53_record" "my_record" {
   zone_id = aws_route53_zone.my_hosted_zone.zone_id
-  name    = "www.mydomain.com"  # Replace with your desired record name
-  type    = "A"
-  ttl     = 300
+  name    = var.record_name  # Replace with your desired record name
+  type    = var.record_type
+  ttl     = var.record_ttl
   records = ["1.2.3.4"]  # Replace with the desired IP address
 }
